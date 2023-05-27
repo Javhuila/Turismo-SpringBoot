139c3e27a6069c2a7e14d6accedd9548fdba1584
Javhuila <80021085+Javhuila@users.noreply.github.com>
2023-05-27 10:21:20 -0500

    Delete repository directory

--- repository/ContactRepository.java
+++
@@ -1,8 +0,0 @@
-package com.product.repository;
-
-import com.product.model.ContactDtls;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactRepository extends JpaRepository<ContactDtls, Integer> {
-
-}
